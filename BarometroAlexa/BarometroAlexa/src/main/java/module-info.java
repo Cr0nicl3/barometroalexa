c5583408ceba977767d9f084790eadc9aa95d87c
Cr0nicl3 <58093415+Cr0nicl3@users.noreply.github.com>
2021-02-11 20:29:21 +0100

    Delete BarometroAlexa/BarometroAlexa directory

--- BarometroAlexa/BarometroAlexa/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.barometrofx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.barometrofx to javafx.fxml;
-    exports com.mycompany.barometrofx;
-}
